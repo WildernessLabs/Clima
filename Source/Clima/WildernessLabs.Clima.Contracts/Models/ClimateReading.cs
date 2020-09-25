82414dfed0ca5125979fabf9941fb03a000cc105
bryan costanich <b@wowzer.net>
2020-09-24 20:04:22 -0700

    Initial cut of apps

---
+++ Source/Clima/WildernessLabs.Clima.Contracts/Models/ClimateReading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Clima.Contracts.Models
+{
+    public class ClimateReading
+    {
+        public ClimateReading()
+        {
+        }
+
+        public long? ID { get; set; }
+        public DateTime TimeOfReading { get; set; }
+        public decimal? TempC { get; set; }
+        public decimal? BarometricPressureMillibarHg { get; set; }
+        public decimal? RelativeHumdity { get; set; }
+    }
+}
---
+++ Source/Clima/WildernessLabs.Clima.Server.Mini/Controllers/ClimateDataController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Clima.Contracts.Models;
+
+namespace Clima.Server.Mini.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClimateDataController : ControllerBase
+    {
+        static Dictionary<long, ClimateReading> ClimateReadings = new Dictionary<long, ClimateReading>();
+
+        static ClimateDataController()
+        {
+            var item = new ClimateReading() {
+                ID = 1,
+                TimeOfReading = DateTime.Now,
+                TempC = 22,
+                BarometricPressureMillibarHg = 200,
+                RelativeHumdity = 0.5m
+            };
+            ClimateReadings.Add((long)item.ID, item);
+        }
+
+        public ClimateDataController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<ClimateReading> Get()
+        {
+            ClimateReading[] readings = new ClimateReading[ClimateReadings.Keys.Count];
+            ClimateReadings.Values.CopyTo(readings, 0);
+            return readings;
+
+            // isn't there an easier way, like thisish?
+            //return _climateReadings.Values.GetEnumerator();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClimateReading>> PostClimateReading(ClimateReading item)
+        {
+            // better way to handle nulls in C# 8 than what i'm doing here?
+            if (item.ID.HasValue && ClimateReadings.ContainsKey(item.ID.Value)) {
+                Conflict("Already exists");
+            }
+
+            // generate an ID
+            item.ID = ClimateReadings.Count + 1;
+            // save the item
+            ClimateReadings.Add((long)(item?.ID), item);
+            return CreatedAtAction(nameof(GetClimateReading), new { id = item.ID }, item);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClimateReading>> GetClimateReading(long id)
+        {
+            if (ClimateReadings.ContainsKey(id)) {
+                return ClimateReadings[id];
+            } else {
+                return NotFound();
+            }
+        }
+
+        //protected long? SaveReading(ClimateReading item)
+        //{
+        //    if (item.ID != null && _climateReadings.ContainsKey((long)item.ID)) {
+        //        return null;
+        //    }
+        //    // generate an ID
+        //    item.ID = _climateReadings.Count;
+        //    // save the item
+        //    _climateReadings.Add((long)(item?.ID), item);
+        //    return item.ID;
+        //}
+    }
+}
