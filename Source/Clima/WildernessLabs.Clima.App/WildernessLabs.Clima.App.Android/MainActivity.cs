fc29c3293cd4b53cba969798dd47d101e10dc352
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-01-07 22:19:56 -0800

    Add SplashScreen for Clima companion app

--- Source/Clima/WildernessLabs.Clima.App/WildernessLabs.Clima.App.Android/MainActivity.cs
+++ Source/Clima/WildernessLabs.Clima.App/WildernessLabs.Clima.App.Android/MainActivity.cs
@@ -5,7 +5,8 @@ using Android.Runtime;
 
 namespace WildernessLabs.Clima.App.Droid
 {
-    [Activity(Label = "WildernessLabs.Clima.App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Clima", Icon = "@mipmap/icon_launcher", RoundIcon = "@mipmap/icon_round_launcher", Theme = "@style/MainTheme",
+     LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -19,7 +20,7 @@ namespace WildernessLabs.Clima.App.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
