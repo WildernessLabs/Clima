7c44e55f0e675b9b374c6e908dc868517b751719
bryan costanich <b@wowzer.net>
2022-08-08 14:47:18 -0700

    add solar voltage check
    Updated the diagnostic app to check the solar voltage input.
    
    Also fixed the namespace of the app.
    
    Note: this builds but i can't test right now, because i can't deploy the OS.

---
+++ Source/Clima/MeadowClimaProKit.Diagnostics/DiagnosticStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeadowClimaProKit.Diagnostics
+{
+    public class DiagnosticStatus
+    {
+        public bool BmeWorking { get; set; } = false;
+        public bool SolarWorking { get; set; } = false;
+
+        public bool AllWorking {
+            get => BmeWorking && SolarWorking;
+        }
+
+        public DiagnosticStatus()
+        {
+        }
+    }
+}
