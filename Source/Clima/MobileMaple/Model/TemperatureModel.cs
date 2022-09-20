ef7da2e55282d00f91f32f2788255c0b3b570d8b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-09-19 19:16:04 -0700

    Add MAUI app (WIP)

---
+++ Source/Clima/MobileMaple/Model/TemperatureModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MobileMaple.Model
+{    
+    [JsonObject]
+    public class TemperatureModel
+    {
+        [JsonProperty("dateTime")]
+        public string DateTime { get; set; }
+        [JsonProperty("temperature")]
+        public string Temperature { get; set; }
+    }
+}
