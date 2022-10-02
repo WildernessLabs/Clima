14faaf0f79541095cb812a8cd8468bfa1c3c149e
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-10-02 15:01:28 -0700

    General cleanup to the Diagnostics app

--- Source/Clima/MeadowClimaProKit.Diagnostics/DiagnosticStatus.cs
+++ Source/Clima/MeadowClimaProKit.Diagnostics/DiagnosticStatus.cs
@@ -1,14 +1,12 @@
-﻿using System;
-
-namespace MeadowClimaProKit.Diagnostics
+﻿namespace MeadowClimaProKit.Diagnostics
 {
     public class DiagnosticStatus
-    {
-        public bool BmeWorking { get; set; } = false;
+    {        
         public bool SolarWorking { get; set; } = false;
 
-        public bool AllWorking {
-            get => BmeWorking && SolarWorking;
+        public bool AllWorking 
+        {
+            get => SolarWorking;
         }
 
         public DiagnosticStatus()
