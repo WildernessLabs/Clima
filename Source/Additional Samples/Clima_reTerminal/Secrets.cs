481666fb54a0bfb5d70e421f8f73903a381b644e
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-05-18 11:28:10 -0700

    Add file

---
+++ Source/Additional Samples/Clima_reTerminal/Secrets.cs
@@ -0,0 +1,49 @@
+﻿namespace Clima_reTerminal;
+
+public class Secrets
+{
+    /// <summary>
+    /// Digital Twin Client Id
+    /// </summary>
+    public const string CLIENT_ID = "CLIENT_ID";
+
+    /// <summary>
+    /// Digital Twin Client Secret
+    /// </summary>
+    public const string CLIENT_SECRET = "CLIENT_SECRET";
+
+    /// <summary>
+    /// Digital Twin Tenant Id
+    /// </summary>
+    public const string TENANT_ID = "TENANT_ID";
+
+    /// <summary>
+    /// Digital Twin Endpoint
+    /// </summary>
+    public const string DIGITAL_TWIN_ENDPOINT = "DIGITAL_TWIN_ENDPOINT";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public const string DIGITAL_TWIN_ID = "DIGITAL_TWIN_ID";
+
+    /// <summary>
+    /// Meadow Cloud URL
+    /// </summary>
+    public const string MEADOW_CLOUD_URL = "MEADOW_CLOUD_URL";
+
+    /// <summary>
+    /// Organization ID - Found in Your Organizations section
+    /// </summary>
+    public const string ORGANIZATION_ID = "ORGANIZATION_ID";
+
+    /// <summary>
+    /// Device ID - Found under Devices section
+    /// </summary>
+    public const string DEVICE_ID = "DEVICE_ID";
+
+    /// <summary>
+    /// API KEY - Found under Your APY keys section
+    /// </summary>
+    public const string API_KEY = "API_KEY";
+}
